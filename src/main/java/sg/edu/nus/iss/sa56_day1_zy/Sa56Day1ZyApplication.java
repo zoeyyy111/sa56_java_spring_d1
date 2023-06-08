182069571b26ec153162e881d61f8773dc15023c
ZOEY_LAPTOP\yaobi <yaobin93113@gmail.com>
2023-06-08 14:58:16 +0800

    1.1 commit

---
+++ src/main/java/sg/edu/nus/iss/sa56_day1_zy/Sa56Day1ZyApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.sa56_day1_zy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sa56Day1ZyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sa56Day1ZyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/sa56_day1_zy/Sa56Day1ZyApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.sa56_day1_zy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sa56Day1ZyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
